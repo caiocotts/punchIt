cf38d9990a96515c7f1bc836e20957f57fc20279
caio <caio@cotts.com.br>
2022-03-04 22:29:21 -0500

    Implement main screen.

---
+++ lib/second.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Second extends StatelessWidget {
+  const Second({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text("Second Screen");
+  }
+}
